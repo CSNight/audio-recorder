bfc02e2cbc788783ba1f670678163b8149fbcc3b
csnight <923739637@qq.com>
2026-06-29 12:13:48 +0800

    feat: codecs chunked encode clean

--- src/codecs/g711/index.ts
+++ src/codecs/g711/index.ts
@@ -1,5 +1,2 @@
 export { g711SnapshotEncoderDefinition } from "./g711-snapshot-exporter"
-export type {
-  G711ExportOptions,
-  G711ExportResult,
-} from "./types"
+export type { G711ExportOptions, G711ExportResult } from "./types"
